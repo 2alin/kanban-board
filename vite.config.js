740870b0d678130c4065e046c2db0b2f0f47cc35
Adilson Sandoval <adilsonsdev@gmail.com>
2025-10-12 22:31:36 +0200

    fix: vite config for github pages [noissue]

--- vite.config.js
+++ vite.config.js
@@ -3,9 +3,10 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
+  base: "/kanban-board/",
   root: "./src",
   build: {
     outDir: "../dist",
-    emptyOutDir: true
-  }
+    emptyOutDir: true,
+  },
 });
