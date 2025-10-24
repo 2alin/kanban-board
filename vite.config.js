3f65af0b66ab50aced995c4eb7912e9804b2eb8d
Adilson Sandoval <adilsonsdev@gmail.com>
2025-10-24 21:32:30 +0200

    chore: remove slash character in base directory [noissue]

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  base: "/kanban-board/",
+  base: "/kanban-board",
   root: "./src",
   build: {
     outDir: "../dist",
