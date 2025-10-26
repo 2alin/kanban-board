4bc4d2c307dd167d8bbaf91880b20c37617fb854
Adilson Sandoval <adilsonsdev@gmail.com>
2025-10-26 16:38:47 +0100

    add initial onboarding card

--- src/defaultSettings.ts
+++ src/defaultSettings.ts
@@ -16,5 +16,21 @@ export const defaultTheme = themes[1];
 export const defaultBoardData: BoardData = {
   version: "0.1",
   categories: ["backlog", "todo", "today", "done"],
-  entries: [],
+  entries: [
+    {
+      categoryIdx: 0,
+      category: "backlog",
+      title: "Welcome to your Kanban Board",
+      description:
+        "This is an example of a card that you can have in this board.\n\n" +
+        "To add a card just click on the big plus sign at the top left of this board.\n\n" +
+        "The card content description support rich text ✨\n\n" +
+        "✅ For bold text, wrap the text with double star or double underscore characters: `**bold**` or `__bold__` will be shown as **bold**.\n" +
+        "✅ For italic text,  wrap the text with single star or single underscore characters: `*italic*` or `_italic_` will be shown as *italic*.\n" +
+        "✅ For unformatted text,  wrap the text with single grave accent characters: ``code`` will be shown as `code`.\n" +
+        "✅ For links, just write the full URL of the link: `https://example.com` will be shown as https://example.com\n\n" +
+        "I hope that this app is helpful for you .\n" +
+        "Enjoy!\n",
+    },
+  ],
 };
