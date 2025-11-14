bff4d6825481f74d70617a84f059c05ea7a30b69
Adilson Sandoval <adilsonsdev@gmail.com>
2025-11-14 18:07:32 +0100

    add toCardsMap test

---
+++ test/data/extendedCards.ts
@@ -0,0 +1,32 @@
+import type { CardExtendedData } from "../../src/components/app.types";
+
+export const cards: CardExtendedData[] = [
+  {
+    id: "card-1",
+    title: "Card 1",
+    description: "Description of Card 1",
+    categoryIdx: 0,
+    orderInCategory: 0,
+  },
+  {
+    id: "card-2",
+    title: "Card 2",
+    description: "Description of Card 2",
+    categoryIdx: 1,
+    orderInCategory: 0,
+  },
+  {
+    id: "card-3",
+    title: "Card 3",
+    description: "Description of Card 3",
+    categoryIdx: 1,
+    orderInCategory: 1,
+  },
+  {
+    id: "card-4",
+    title: "Card 4",
+    description: "Description of Card 4",
+    categoryIdx: 2,
+    orderInCategory: 0,
+  },
+];
