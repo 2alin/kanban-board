ea0d811b1b208fc0221cd07983c65b812a9ce5fd
Adilson Sandoval <adilsonsdev@gmail.com>
2025-11-20 00:21:34 +0100

    add vitest browser mode configuration

--- vite.config.ts
+++ vite.config.ts
@@ -1,4 +1,6 @@
+/// <reference types="vitest/config" />
 import { defineConfig } from "vite";
+import { playwright } from "@vitest/browser-playwright";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
@@ -8,5 +10,31 @@ export default defineConfig({
   build: {
     outDir: "../dist",
     emptyOutDir: true,
+  },
+  test: {
+    projects: [
+      {
+        test: {
+          include: [
+            "./**/*.browser.test.{ts,tsx}",
+            "vitest-example/*.test.tsx",
+          ],
+          name: "browser",
+          browser: {
+            enabled: true,
+            provider: playwright(),
+            // https://vitest.dev/guide/browser/playwright
+            instances: [{ browser: "chromium" }],
+          },
+        },
+      },
+      {
+        test: {
+          include: ["./**/*.unit.test.{ts,tsx}"],
+          name: "unit",
+          environment: "node",
+        },
+      },
+    ],
   },
 });
