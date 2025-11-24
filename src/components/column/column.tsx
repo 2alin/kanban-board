7dceb3e9a877f88c5339610fc68ccd95304c6a0f
Adilson Sandoval <adilsonsdev@gmail.com>
2025-11-24 21:43:56 +0100

    added new column functionality

--- src/components/column/column.tsx
+++ src/components/column/column.tsx
@@ -2,12 +2,16 @@ import "./column.css";
 
 import { useContext, useRef, useState } from "react";
 
-import { CategoriesDispatchContext } from "../../contexts/categories";
+import {
+  CategoriesContext,
+  CategoriesDispatchContext,
+} from "../../contexts/categories";
 
 import type { CardExtendedData, ModalState } from "../app.types";
 import Card from "../card";
 import Menu from "../shared/menu";
 import TitleEditForm from "./titleEditForm";
+import { CardsContext, CardsDispatchContext } from "../../contexts/cards";
 
 interface ColumnProps {
   columnId: number;
@@ -29,7 +33,11 @@ export default function Column({
   const [isTitleEdit, setIsTitleEdit] = useState(false);
   const columnRef = useRef<HTMLElement>(null);
 
+  const categories = useContext(CategoriesContext);
   const categoriesDispatch = useContext(CategoriesDispatchContext);
+
+  const boardCards = useContext(CardsContext);
+  const cardsDispatch = useContext(CardsDispatchContext);
 
   function handleMenuClick(event: React.MouseEvent) {
     const { target } = event;
@@ -45,21 +53,67 @@ export default function Column({
         setIsTitleEdit(true);
         break;
       case "add-card":
-        {
-          let origin =
-            columnRef.current?.querySelector(".menu-component .anchor") ||
-            undefined;
+        let origin =
+          columnRef.current?.querySelector(".menu-component .anchor") ||
+          undefined;
 
-          if (!(origin instanceof HTMLElement)) {
-            origin = undefined;
-          }
-
-          setModalState({
-            type: "new",
-            categoryIdx: columnId,
-            origin,
-          });
+        if (!(origin instanceof HTMLElement)) {
+          origin = undefined;
         }
+
+        setModalState({
+          type: "new",
+          categoryIdx: columnId,
+          origin,
+        });
+
+        break;
+      case "add-column-right":
+      case "add-column-left":
+        // positiion is "right" or "left"
+        const newColumnPosition = key.replace("add-column-", "");
+        let newColumnIdx: number;
+
+        if (newColumnPosition === "right") {
+          newColumnIdx = columnId + 1;
+        } else if (newColumnPosition === "left") {
+          newColumnIdx = columnId - 1;
+        } else {
+          console.error(
+            "[add column] Unrecognized new column position: ",
+            newColumnPosition
+          );
+          break;
+        }
+
+        if (newColumnIdx < 0) {
+          // minimum index should be zero
+          newColumnIdx = 0;
+        } else if (newColumnIdx > categories.length) {
+          // maximum index should be a unit increment on the current list of columns
+          newColumnIdx = categories.length;
+        }
+
+        const newColumnTitle = "New Column";
+        const newCategories = [...categories];
+        newCategories.splice(newColumnIdx, 0, newColumnTitle);
+        categoriesDispatch({ type: "set", categories: newCategories });
+
+        if (newColumnIdx < categories.length && boardCards.length > 0) {
+          // we need to move cards of categories that will change
+          const newBoardCards: CardExtendedData[] = boardCards.map((card) => {
+            const newCard = structuredClone(card);
+
+            if (newCard.categoryIdx >= newColumnIdx) {
+              newCard.categoryIdx++;
+            }
+
+            return newCard;
+          });
+
+          cardsDispatch({ type: "set", cards: newBoardCards });
+        }
+
         break;
       default:
       // nothing to do here
@@ -93,6 +147,16 @@ export default function Column({
                 {
                   key: "edit-column-name",
                   text: "Edit Title",
+                  handler: handleMenuClick,
+                },
+                {
+                  key: "add-column-right",
+                  text: "Add Column to the right",
+                  handler: handleMenuClick,
+                },
+                {
+                  key: "add-column-left",
+                  text: "Add Column to the left",
                   handler: handleMenuClick,
                 },
                 {
