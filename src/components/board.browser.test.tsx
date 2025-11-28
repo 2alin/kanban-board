2278cc8e902a4a8efde3349f2c7f713cef511a91
Adilson Sandoval <adilsonsdev@gmail.com>
2025-11-28 16:24:57 +0100

    add new test to 'add column' feature

--- src/components/board.browser.test.tsx
+++ src/components/board.browser.test.tsx
@@ -4,6 +4,8 @@ import { page } from "vitest/browser";
 import Board from "./board";
 import { CategoriesProvider } from "../contexts/categories";
 import { CardsProvider } from "../contexts/cards";
+import type { CardExtendedData } from "./app.types";
+import { cards } from "../../test/data/extendedCards";
 
 vi.mock("../storage", () => {
   return {
@@ -19,13 +21,18 @@ vi.mock("../storage", () => {
   };
 });
 
-function renderEmptyColumn(columnTitle: string) {
+const cardFirstcolumn = cards[0];
+
+function renderBoardWithColumn(
+  columnTitle: string,
+  cards: CardExtendedData[] = []
+) {
   return render(
     <CategoriesProvider
       initialCategories={[columnTitle]}
       handleHistoryChange={() => {}}
     >
-      <CardsProvider initialCards={[]} handleHistoryChange={() => {}}>
+      <CardsProvider initialCards={cards} handleHistoryChange={() => {}}>
         <Board
           handlers={{
             setModalState: () => {},
@@ -40,7 +47,7 @@ function renderEmptyColumn(columnTitle: string) {
 
 describe("Board Component", () => {
   it("should create a new column ahead", async () => {
-    await renderEmptyColumn("Test Column");
+    await renderBoardWithColumn("Test Column");
 
     const columns = page.getByRole("region", { name: "column" });
     expect(columns.elements().length).toBe(1);
@@ -65,7 +72,7 @@ describe("Board Component", () => {
   });
 
   it("should create a new column behind", async () => {
-    await renderEmptyColumn("Test Column");
+    await renderBoardWithColumn("Test Column");
 
     const columns = page.getByRole("region", { name: "column" });
     expect(columns.elements().length).toBe(1);
@@ -86,6 +93,33 @@ describe("Board Component", () => {
       .toBeInTheDocument();
     await expect
       .element(columns.nth(1).getByText("Test Column"))
+      .toBeInTheDocument();
+  });
+
+  it("should move cards when adding a column", async () => {
+    await renderBoardWithColumn("Test Column", [
+      structuredClone(cardFirstcolumn),
+    ]);
+
+    const columnMenuButton = page.getByRole("button", {
+      name: "Column options menu",
+    });
+    await columnMenuButton.click();
+
+    const addColumnRightButton = page.getByRole("menuitem", {
+      name: "Add Column Behind",
+    });
+    await addColumnRightButton.click();
+
+    const columns = page.getByRole("region", { name: "column" });
+
+    await expect
+      .element(columns.nth(0).getByRole("listitem"))
+      .not.toBeInTheDocument();
+    await expect
+      .element(
+        columns.nth(1).getByRole("listitem", { hasText: cardFirstcolumn.title })
+      )
       .toBeInTheDocument();
   });
 });
