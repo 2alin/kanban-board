dd04338585f617a5deea14ca04e82b4815bfcd6d
Adilson Sandoval <adilsonsdev@gmail.com>
2026-02-01 00:46:42 +0100

    allow badge to be empty

--- src/components/column/counterBadge.tsx
+++ src/components/column/counterBadge.tsx
@@ -4,11 +4,21 @@ interface CounterBadgeProps {
   amount: number;
 }
 export default function CounterBadge({ amount }: CounterBadgeProps) {
+  const maxAmountToDisplay = 99;
+  let textToDisplay: string;
+
   if (amount <= 0) {
-    return;
+    // let's render the badge even if there's no amount
+    // to avoid space tittle jumps when badge amount changes
+    textToDisplay = "";
+  } else {
+    textToDisplay =
+      amount > maxAmountToDisplay
+        ? `${maxAmountToDisplay}+`
+        : amount.toString();
   }
 
-  const textToDisplay = amount > 99 ? "99+" : amount.toString();
-
-  return <span className="badge">{textToDisplay}</span>;
+  return (
+    <span className={`badge ${amount <= 0 && "empty"}`}>{textToDisplay}</span>
+  );
 }
